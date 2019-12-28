6fdf5a83e2d6438c24c69d8fb542deb180c8b80c
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2019-12-28 23:46:40 +0100

    Create page280socket_server_forking.py

---
+++ chap14/page280socket_server_forking.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.6
+
+from socketserver import TCPServer, StreamRequestHandler, ForkingMixIn
+
+class forkingServer(ForkingMixIn, TCPServer):
+    pass
+
+class ClientHandler(StreamRequestHandler):
+    def handle(self):
+        addr = self.request.getpeername()
+        msg_to_client = 'Thank you for connecting to forking SocketServer'
+        print('Got connection from', addr)
+        self.wfile.write(bytes(msg_to_client, 'utf-8'))
+
+listening_port = 1234        
+try:
+    socket_server = forkingServer(('', listening_port), ClientHandler)
+    print("Forking socketServer started, listening on port ",listening_port)
+    print("...[stop by Ctrl+C]")
+    socket_server.serve_forever()
+except Exception as setuper:
+    print(" Could not start forking server, due to :", setuper)
+except KeyboardInterrupt:
+    print("Server was stopped by a Ctrl+C. Exiting program")
