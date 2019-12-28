39393c78893af82deadb3c978a9bcb056340b9b0
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2019-12-28 19:39:08 +0100

    Update page275.simple_client.py

--- chap14/page275.simple_client.py
+++ chap14/page275.simple_client.py
@@ -4,11 +4,11 @@ import socket
 
 client_socket = socket.socket()
 server_host = socket.gethostname() # here the server is the same PC as the client
-# 
+#server_host = '192.168.122.10' ## if you have setup a KVM (or a real outisde machine) and the server program runs there
 port_to_connect_on_server = 1234
 receiving_packets_size = 1024
 try:
     client_socket.connect((server_host, port_to_connect_on_server))
     print(client_socket.recv(receiving_packets_size))
-except Exception as er:
-    print("Failed to retreive info due to error:", er)
+except Exception as coner:
+    print("Failed to retreive info due to error:", coner)
